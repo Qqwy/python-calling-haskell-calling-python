822eb1c351d449a487be9d586b760c7f507d1ae0
qqwy <qqwy@gmx.com>
2024-04-26 23:13:16 +0200

    Even with extra libraries and threading it works!

--- src/Ouroboros.hs
+++ src/Ouroboros.hs
@@ -4,10 +4,15 @@ import qualified Data.Char as C
 import Foreign.C.Types
 import Foreign.C.String
 
+import Control.Concurrent.Async (async, wait)
+
 example :: CString -> IO CString 
 example cstr = do
   charlist <- peekCString cstr
   let appended = charlist ++ " example!!"
+  promise <- async $ do
+    putStrLn $ "The result is: " <> appended
+  wait promise
   newCString appended
 
 foreign export ccall example :: CString -> IO CString
