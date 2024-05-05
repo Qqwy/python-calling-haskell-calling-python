fd3dabf9415d10798e9ab3bf94b66e80e399ef35
qqwy <qqwy@gmx.com>
2024-05-05 13:54:44 +0200

    Clean up old files

--- src/Ouroboros.hs
+++ src/Ouroboros.hs
@@ -4,49 +4,22 @@
 {-# LANGUAGE GHC2021 #-}
 {-# LANGUAGE RecordWildCards #-}
 module Ouroboros where
-import qualified Data.Char as C
-import Foreign.C.Types
-import Foreign.C.String
-import Foreign.C.Error
 import Foreign.Ptr
 import Foreign.Marshal
-import Foreign.Storable
-
-import Control.Monad (forever)
-import Control.Concurrent (threadDelay, yield)
-import Control.Concurrent.Async (async, wait, race, withAsync, cancel, forConcurrently)
-import Data.Vector.Storable (Vector)
-import qualified Data.Vector.Storable as SVector
--- import Control.Exception
 
 import Data.Function ((&))
 import Data.Functor ((<&>))
 import qualified Control.Exception as E
-import Control.Concurrent
-import System.Posix.Signals
-import Foreign.Storable.Tuple
-import SizePrefixedArray (SizePrefixedArray)
-import SizePrefixedArray qualified
-import FatPtr (FatPtr)
-import FatPtr qualified
-import Data.Word
-import GHC.IsList (IsList, Item)
-import GHC.IsList qualified as IsList
 import Data.ByteString (ByteString)
-import Data.ByteString qualified as ByteString
 import Data.ByteString.Lazy qualified as ByteString.Lazy
 import ByteBox (ByteBox)
 import ByteBox qualified
 import Data.Aeson qualified as Aeson
-import qualified Type.Reflection as Reflection
 import qualified Data.Bifunctor as Bifunctor
-import Control.Exception.Annotated (AnnotatedException)
 import Control.Exception.Annotated.UnliftIO
 import UnliftIO.Exception (evaluateDeep, handle)
 import Control.DeepSeq (NFData)
 import GHC.Stack
-import qualified Data.Annotation as Annotation
-import Data.Maybe (maybeToList)
 import SerializedExceptions
 
 -- | Export `reallocBytes` so the foreign (in this case Python) code
